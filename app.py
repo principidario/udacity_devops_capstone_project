c47477a93652d215ad94a684bbfa9dfcde04d275
Dario Principi <principidario@gmail.com>
2022-04-11 16:31:41 +0200

    commit

--- app.py
+++ app.py
@@ -17,23 +17,20 @@ def allowed_file(filename):
 @app.route("/run", methods=['POST'])
 def run():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if 'file' in request.files:
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                #Run ML model to predict Oxygen Saturation in Arterial Blood
+                return "SPO2: 98%"
+            else:
+                return ""
+        else:
+            return ""
+    else:
+        return ""
             
-            #Run ML model to predict Oxygen Saturation in Arterial Blood
-            
-            return "SPO2: 98%"
 
 if __name__ == "__main__":
     #load model
